56ac7a0cbe57bcef7ff9323ea2e64e948622a42f
nexx01@mail.ru <@Oneplus@>
2022-04-04 22:30:52 +0300

    CompletableFuturePractice

---
+++ src/main/java/Async/CompletableFuturePractice.java
@@ -0,0 +1,69 @@
+package Async;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public class CompletableFuturePractice {
+
+    public static class PriceRetriever {
+
+        public double getPrice(long itemId, long shopId) {
+            // имитация долгого HTTP-запроса
+            int delay = ThreadLocalRandom.current().nextInt(10);
+            try {
+                Thread.sleep(delay * 1000);
+            } catch (InterruptedException e) {
+            }
+
+            return ThreadLocalRandom.current().nextDouble(1000);
+        }
+    }
+
+    public static class PriceAggregator {
+
+        private PriceRetriever priceRetriever = new PriceRetriever();
+
+        private Set<Long> shopIds = Set.of(10l, 45l, 66l, 345l, 234l, 333l,
+                67l, 123l, 768l);
+
+        public double getMinPrice(long itemId) {
+            // здесь будет ваш код
+            int someItemId = 1;
+            int maxTimeWaitingMillis = 2900;
+
+            List<CompletableFuture<Double>> collect = shopIds.stream()
+                    .map(id -> CompletableFuture.supplyAsync(
+                                    () -> priceRetriever.getPrice(someItemId, id))
+                            .completeOnTimeout(Double.MAX_VALUE,maxTimeWaitingMillis,TimeUnit.MILLISECONDS)
+                    )
+                    .collect(Collectors.toList());
+
+            double defaultMinValue = 0.00;
+
+            return CompletableFuture.allOf(collect.toArray(new CompletableFuture[0]))
+                    .thenApply(v -> collect.stream()
+                            .map(CompletableFuture::join)
+                            .filter(pr->pr<Double.MAX_VALUE)
+                            .min(Double::compareTo)
+                            .orElse(defaultMinValue))
+                    .join();
+        }
+    }
+
+    public static void main(String[] args) {
+        PriceAggregator priceAggregator = new PriceAggregator();
+        long itemId = 12l;
+
+        long start = System.currentTimeMillis();
+        double min = priceAggregator.getMinPrice(itemId);
+        long end = System.currentTimeMillis();
+
+        System.out.println(min);
+        System.out.println((end - start) < 3000); // should be true
+    }
+}
+
